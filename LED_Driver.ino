577aaa520b4e6fe0024bb1641c612ddd36671a09
LuPro <LuPro@users.noreply.github.com>
2016-12-25 15:25:40 +0100

    Add files via upload
    Added the first iteration of the LED driver for the KASTL. Does not include serial readout right now.

---
+++ LED_Driver.ino
@@ -0,0 +1,37 @@
+#include "colorHandler.h"
+
+void setup() {
+  //Initialize Serial connection, only for debug purposes
+  Serial.begin(9600);
+}
+
+void loop() {
+  static ColorHandler lights;   //ColorHandler handles everything related to LED strips and color functions
+
+  //RGBa consists of 4 channels (in this order): 8b alpha, 8b blue, 8b green, 8b red
+  //color consists of 3 channels (in this order): 8b blue, 8b green, 8b red
+  uint32_t RGBa, color;
+
+  //If there is serial data available, read this data
+  //available means new (unread as of yet) data in the dedicated serial buffer (buffer size default: 64 byte)
+  if (Serial.available()) {
+    lights.getRGBaSerial();
+  }
+
+  //if RGBa is clean, the value can be extracted
+  //clean means, that the complete RGBa value is received and stored. If this would not be here, the could be cases where
+  //the RGBa value is used even if only half of it got transmitted via serial connection. This ensures data integrity
+  if (lights.RGBaIsClean()) {
+    RGBa = lights.getRGBa();
+  }
+
+  RGBa = lights.generateRGBa (127, 127, 127, 127);    //generate a test color, only for debug
+
+  color = lights.RGBa_to_RGB(RGBa);
+  lights.colorWipe (color, top);
+
+  //use this delay only if debugging via serial interface - with this delay you can read the individual debug infos, but
+  //the program won't work properly if this is left for normal use
+  delay(500);
+}
+
---
+++ colorHandler.cpp
@@ -0,0 +1,62 @@
+#include "colorHandler.h"
+
+//converts RGBa color to RGB color
+uint32_t ColorHandler::RGBa_to_RGB (uint32_t RGBa) {
+  uint32_t val_RGB = 0;
+  uint8_t r, g, b, alpha;
+
+  //gets the individual channels
+  r = getColorChannel (chR, RGBa);
+  g = getColorChannel (chG, RGBa);
+  b = getColorChannel (chB, RGBa);
+  alpha = getColorChannel (chAlpha, RGBa);
+
+  //scales the individual color channels by the alpha channel
+  r *= (alpha / 255.0);
+  g *= (alpha / 255.0);
+  b *= (alpha / 255.0);
+
+  //stores the resulting RGB color
+  val_RGB = b;
+  val_RGB = val_RGB << 8;
+  val_RGB |= g;
+  val_RGB = val_RGB << 8;
+  val_RGB |= r;
+  
+  return val_RGB;
+}
+
+//flushed a desired LED strip with a desired RGB color, use Strips enum to choose which strip to use
+void ColorHandler::colorWipe (uint32_t color, uint8_t stripPos) {
+  for (uint8_t i = 0; i < strips[stripPos].numPixels(); i++) { //iterates through every pixel of the LED strip
+    strips[stripPos].setPixelColor (i, color);
+  }
+  strips[stripPos].show(); //shows the LED strip. Internal function of the Adafruit_Neopixel library. Is used to actually apply the color info
+}
+
+//generates a RGBa color, is only used for debugging purposes, could be used later on for advanced strip effects as a helper function
+uint32_t ColorHandler::generateRGBa (uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) {
+  uint32_t RGBa = 0;
+
+  //shifts the individual channels on the right position
+  RGBa = alpha;
+  RGBa = RGBa << 8;
+  RGBa |= b;
+  RGBa = RGBa << 8;
+  RGBa |= g;
+  RGBa = RGBa << 8;
+  RGBa |= r;
+  
+  return RGBa;
+}
+
+//does nothing as of yet, should get the RGBa data via serial. Wether there is a Raspberry Pi or not changes if this function has to
+//be able to decipher the whole data protocol defined by BCH and SAL, or just the Lights part
+uint32_t ColorHandler::getRGBaSerial () {
+  return 1;
+}
+
+//returns a desired channel of a RGBa color, use the Channels enum for selection of the channel
+uint8_t ColorHandler::getColorChannel (uint8_t channel, const uint32_t &RGBa) {
+  return (RGBa >> (8*channel)) & B11111111;
+}
